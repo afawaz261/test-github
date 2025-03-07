8aac79eb3c4682604b554a912b23f11782a82b85
rozanwehbe <rozan.wehbe.96@gmail.com>
2025-03-07 15:42:39 +0200

    merge

--- ali.py
+++ ali.py
@@ -2,7 +2,8 @@
 # I'm Rozanne 
 print("rosie")
 
+print("wahahahahah")
+
 "kobeissi is not going to fail us"
 "rida is going not to laugh at us"
 print("I am so enjoying this, waw")# Im yara
-print ("yara")
